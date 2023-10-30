926529670f26f87f795e2f90f1618e3748239a8c
Austin <austin.amaral3@gmail.com>
2023-10-30 16:29:28 -0700

    add devs and setup server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
